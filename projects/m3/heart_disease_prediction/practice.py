7627c56e6344800bafcfe330cbf82e3d206a0db4
Hieu Tran <jrvn-hieu@Hieus-MacBook-Pro.local>
2024-10-02 10:38:46 +0700

    add feature important and some note to heart disease prediction

--- projects/m3/heart_disease_prediction/practice.py
+++ projects/m3/heart_disease_prediction/practice.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import confusion_matrix
 from sklearn.svm import SVC
@@ -11,6 +12,8 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier, GradientBoostingClassifier, StackingClassifier
 from xgboost import XGBClassifier
+
+from sklearn import datasets
 
 from dataset_man import dataset_manager
 
@@ -43,11 +46,18 @@ def main():
     print(df.head())
 
     # bt1
+    sns.set_context("paper", font_scale = 1, rc = {"font.size": 3,"axes.titlesize": 15,"axes.labelsize": 10})
+
     plt.figure(figsize=(6, 4))
-    sns.countplot(data=df, x='age', hue='target')
-    plt.xticks(['29', '39', '44', '49', '54', '59', '64', '69', '77'])
+    sns.countplot(data=df, x='age', hue='target', order = df['age'].sort_values().unique())
+    plt.xticks(np.arange(0, 80, 5))
     plt.title('Variation of Age for each target class')
     plt.show()
+
+    # ax = sns.catplot(kind = 'count', data = df, x = 'age', hue = 'target', order = df['age'].sort_values().unique())
+    # ax.ax.set_xticks(np.arange(0, 80, 5))
+    # plt.title('Variation of Age for each target class')
+    # plt.show()
 
     # bt2
     plt.figure(figsize=(6, 4))
@@ -61,6 +71,30 @@ def main():
     y = df.iloc[:, -1].values
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=42)
+    
+    # Preprocessing
+    # sc = StandardScaler()
+    # X_train = sc.fit_transform(X_train)
+    # X_test = sc.transform(X_test)
+    # fit_transform
+    # This method is applied to the training data (X_train).
+    # It performs two operations:
+    # Fit: The StandardScaler calculates the mean and standard deviation of the training data.
+    # Transform: It then uses these statistics (mean and standard deviation) to scale the data by subtracting the mean and dividing by the standard deviation.
+    # This ensures that the training data is standardized based on its own distribution.
+    
+    # transform
+    # This method is applied to the test data (X_test).
+    # It only transforms the data using the mean and standard deviation that were calculated from the training data during the fit process.
+    # The test data is not refitted (i.e., the mean and standard deviation are not recalculated).
+    # Instead, the scaling applied to the test data is consistent with the training data,
+    # ensuring that the model's assumptions remain valid when applied to unseen data.
+
+    # Some models (e.g., tree-based models like Decision Trees, Random Forest, or XGBoost) do not require feature scaling.
+    # Tree-based models are generally invariant to the scaling of the input features, and scaling might even hurt performance
+    # because these models rely on splitting based on feature values.
+    # In contrast, models like SVM, KNN, and Logistic Regression are more sensitive to feature scales.
+    # If you are using a tree-based model, applying StandardScaler might have unnecessarily transformed the data, causing a drop in accuracy.
 
     knn_cls = KNeighborsClassifier(n_neighbors=5, weights='uniform', algorithm='auto', leaf_size=30, p=2, metric='minkowski')
     knn_cls.fit(X_train, y_train)
@@ -110,11 +144,34 @@ def main():
     ad = AdaBoostClassifier(random_state=42)
     xgb = XGBClassifier()
 
-    cls_lst = [('dtc', dtc), ('rfc', rfc), ('knn', knn), ('gc', gc), ('svc', svc), ('ad', ad)] 
+    cls_lst = [('dtc', dtc), ('rfc', rfc), ('knn', knn), ('gc', gc), ('ad', ad), ('svc', svc)] 
 
     stacking_cls = StackingClassifier(estimators=cls_lst, final_estimator=xgb)
     stacking_cls.fit(X_train, y_train)
     predict_and_display_accurcy_for_train_and_test_data(X_train, X_test, y_train, y_test, stacking_cls)
+
+    # Feature important
+    dataset = datasets.load_breast_cancer()
+    X = pd.DataFrame(dataset.data, columns=dataset.feature_names)
+    y = dataset.target
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25)
+
+
+    clf = DecisionTreeClassifier(criterion='gini')
+    # Fit the decision tree classifier
+    clf = clf.fit(X_train, y_train)
+    # Print the feature importances
+    feature_importances = clf.feature_importances_
+
+    # Sort the feature importances from greatest to least using the sorted indices
+    sorted_indices = feature_importances.argsort()[::-1]
+    sorted_feature_names = dataset.feature_names[sorted_indices]
+    sorted_importances = feature_importances[sorted_indices]
+
+    # Create a bar plot of the feature importances
+    sns.set_theme(rc={'figure.figsize':(11.7,8.27)})
+    sns.barplot(x = sorted_importances, y = sorted_feature_names)
+    plt.show()
 
 if __name__ == "__main__":
     main()
